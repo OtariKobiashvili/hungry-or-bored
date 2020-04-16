292f53a3bba9817d617d4989f52c100163300b30
Otari Kobiashvili <otarikobi@gmail.com>
2020-04-15 22:45:21 -0400

    inital commit

---
+++ src/view/root.tsx
@@ -0,0 +1,32 @@
+import '../style/root.scss';
+
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { Store } from 'redux';
+
+interface RootProps {
+  store: Store;
+}
+
+export const Root: React.FC<RootProps> = (props) => {
+  return (
+    <Provider store={props.store}>
+      <Router>
+        <div className="root">
+          <h1>Works</h1>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="About">About</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </Router>
+    </Provider>
+  );
+}
